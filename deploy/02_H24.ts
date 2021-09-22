071d2e7a57a0ed9764a7b38c6dcd2646b140d4b1
svinerus <svinerus@gmail.com>
2021-09-22 18:13:04 +0300

    Add rinkeby WBTC contract address

--- deploy/02_H24.ts
+++ deploy/02_H24.ts
@@ -14,9 +14,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 };
 
 async function getWBTCAddress(hre: HardhatRuntimeEnvironment) {
-  if (hre.network.name != "hardhat")
+  if (hre.network.name == "hardhat")
+    return (await hre.ethers.getContract("WBTCToken")).address;
+  if (hre.network.name == "rinkeby")
+    return "0x577D296678535e4903D59A4C929B718e1D575e0A";
+  if (hre.network.name == "mainnet")
     return "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599";
-  return (await hre.ethers.getContract("WBTCToken")).address;
+  throw "wrong network"
 }
 
 export default func;
